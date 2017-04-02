86746663ab9b1329c772a80bbb959884bba71f1c
kevin95670 <kevinleonard.info@gmail.com>
2017-04-02 16:09:25 +0200

    Add files via upload

--- Projet/php/contact.php
+++ Projet/php/contact.php
@@ -1,41 +1,44 @@
 <?php session_start();?>
 <?php require("../../Projet/html/headerHTML.html"); ?>
-		
-		<?php if (!empty($_SESSION['connecte']))
-		{
-			require("headerConnected.php");
-		}
-		else
-		{
-			require("headerDisconnected.php");
-		}
-		?>
+
+<?php if (!empty($_SESSION['connecte']))
+{
+	require("headerConnected.php");
+}
+else
+{
+	require("headerDisconnected.php");
+}
+?>
 
 
-		<?php
-		/*if($_POST) {
+<?php
+		if($_POST) {
 
-		$emetteur=$_POST['emailContact'];
+
+		$headers = "From:".$_POST['emailContact']. "\r\n";
 		$message=$_POST['comment'];
 
-		mail("kevin-leonard@orange.fr", "contact AcTVty", $message, $emetteur) ;
+		mail("kevin-leonard@orange.fr", "contact AcTVty", $message, $headers) ;
 
-		}*/
-		?>
+	}
+	?>
 
-		<div class="main-content">
+	<div class="main-content">
+		<div class="contact identification">
 			<h1 class="bg-yellow">Contactez-nous via ce formulaire</h1>
 			<form name="contact" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-			<input type="email" name="emailContact" placeholder="Entrez votre adresse mail"><br/>
-			<textarea name="comment" rows="8" cols="50">Entrez votre demande</textarea><br/>
-			<input type="submit" value="envoyer">
+				<p><input type="email" name="emailContact" placeholder="Entrez votre adresse mail"></p>
+				<p><textarea name="comment" rows="8" cols="50">Entrez votre demande.</textarea></p>
+				<p><input type="submit" value="Envoyer"></p>
 			</form>
 		</div>
+	</div>
 
-		<?php require("right-side.php"); ?>
+	<?php require("right-side.php"); ?>
 
-		<?php require("footer.php"); ?>
+	<?php require("footer.php"); ?>
 
-	</body>
+</body>
 
 </html>
--- Projet/php/who.php
+++ Projet/php/who.php
@@ -1,26 +1,30 @@
 <?php session_start();?>
 <?php require("../../Projet/html/headerHTML.html"); ?>
 
-    <?php if (!empty($_SESSION['connecte']))
-		{
-			require("headerConnected.php");
-		}
-		else
-		{
-			require("headerDisconnected.php");
-		}
-		?>
+<?php if (!empty($_SESSION['connecte']))
+{
+	require("headerConnected.php");
+}
+else
+{
+	require("headerDisconnected.php");
+}
+?>
 
-        <div class="main-content">
-            <h1 class="bg-yellow">Qui sommes nous ?</h1>
-            <img src="../../Projet/images/stream.png" width="250" alt="streaming" />
-            <p>Ac<i class="fa fa-television" aria-hidden="true"></i>ty est une site de streaming non officiel où vous pourrez retrouver des centaines de séries.</p>
-        </div>
+<div class="main-content">
+	<div class="who">
+		<h1 class="bg-yellow">Qui sommes nous ?</h1>
+		<img src="../../Projet/images/stream.png" width="250" alt="streaming" />
+		<p>Ac<i class="fa fa-television" aria-hidden="true"></i>ty est une site de streaming non officiel où vous pourrez retrouver des centaines de séries.</p>
+		<h2>Il manque une série? <i class="fa fa-frown-o" aria-hidden="true"></i></h2>
+		<p>N'hésitez surtout pas à nous contacter via ce <a href="contact.php">formulaire</a> pour nous faire part de vos envies au niveau des séries. <i class="fa fa-smile-o" aria-hidden="true"></i></p>
+	</div>
+</div>
 
-        <?php require("right-side.php"); ?>
+<?php require("right-side.php"); ?>
 
-            <?php require("footer.php"); ?>
+<?php require("footer.php"); ?>
 
-	</body>
+</body>
 
 </html>
